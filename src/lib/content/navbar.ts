507344a4fd6f51931c2402bf82f4b92b4d30c32d
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-02-16 01:01:18 +0800

    Update navbar.ts

--- src/lib/content/navbar.ts
+++ src/lib/content/navbar.ts
@@ -3,7 +3,7 @@ import { resumeFileName } from '@/lib/utils/config';
 
 export const navbarSection: NavbarSectionType = {
     navLinks: [
-        { name: 'about', url: '/#about'},
+        { name: 'Certificate', url: '/#about'},
         // { name: 'skills', url: '/#skills'},
         { name: 'experience', url: '/#experience' },
        // { name: 'projects', url: '/#projects' },

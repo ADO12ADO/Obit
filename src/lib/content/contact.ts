bbc800a11a1841ae670414114840f0c7f1714c94
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-02-16 00:53:08 +0800

    Update contact.ts

--- src/lib/content/contact.ts
+++ src/lib/content/contact.ts
@@ -2,11 +2,11 @@ import { author } from '@/lib/content/portfolio';
 import { ContactSectionType } from '@/lib/types/sections';
 
 export const contactSection: ContactSectionType = {
-    title: 'get in touch',
-    subtitle: "what's next",
+    title: 'Contact me',
+    subtitle: "",
     paragraphs: [
-    'I’m currently looking for a remote job or any new opportunities.',
-    'Whether you have a project to discuss or just want to say hi, my inbox is open for all!',
+    'I’m currently looking for a new job or any new opportunities.',
+    'Whether you to discuss or just want to say hi, my inbox is open for all!',
     ],
     link: `mailto:${author.email}`,
 };

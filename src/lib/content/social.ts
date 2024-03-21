bc8ada1019ea3ab9dc7a3066b7b1de1fb49572da
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-03-21 22:26:19 +0800

    Update social.ts

--- src/lib/content/social.ts
+++ src/lib/content/social.ts
@@ -5,16 +5,8 @@ import { SocialSectionType } from '@/lib/types/sections';
 export const socialSection: SocialSectionType = {
     socialLinks: [
     {
-        icon: 'tabler:brand-github',
-        url: socialLinks.github,
-    },
-    {
         icon: 'mdi:instagram',
         url: socialLinks.instagram,
-    },
-    {
-        icon: 'lucide:twitter',
-        url: socialLinks.twitter,
     },
     {
         icon: 'lucide:linkedin',

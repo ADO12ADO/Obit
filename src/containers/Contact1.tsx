8f1515c2eaa6bf78c23feeafd828828756734bed
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-05-08 23:39:23 +0800

    Update Contact1.tsx

--- src/containers/Contact1.tsx
+++ src/containers/Contact1.tsx
@@ -4,7 +4,7 @@ import { Button, Wrapper } from '@/components';
 
 import { getSectionAnimation } from '@/styles/animations';
 
-const Contact = () => {
+const Contact1 = () => {
   const { subtitle, title, paragraphs, link } = contactSection;
   return (
     <Wrapper

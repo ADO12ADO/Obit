be3117aa37706c2debd3dc92b9669d637c060795
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-05-08 22:22:14 +0800

    Update about.ts

--- src/lib/content/about.ts
+++ src/lib/content/about.ts
@@ -1,25 +1,19 @@
 import { AboutSectionType } from '@/lib/types/sections'
 
 export const aboutSection: AboutSectionType = {
-   title: '',
+    title: '',
     // Paragraphs need to be changed from containers/About.tsx
     // Because it wasn't possible to insert anchor tags like this
-   list: {
-   // title: 'Here is a list of professional certificates already working with recently on the ship:',
-   // items: [
-      //  'COC Class 2 Engineer',
-       // 'Basic Safety Training',
-      //  'Survival Craft and Rescue Boats',
-       // 'Advanve Fire Fighting',
-      //  'Medical First Aid',
-      //  'Medical Care',
-      //  'Security Awareness Training',
-        //'Engine Resource Management',
-       // 'Ship Security Officer',
-      //  'Basic Oil and Gas Tanker',
-      //  'Advance Oil Tanker',
-     //   'International Safety Management Code',
-   // ],
+    list: {
+    title: '',
+    items: [
+       // 'NextJs',
+      //  'TailwindCSS',
+       // 'ExpressJs',
+      //  'Unit Testing',
+        //'TypeScript',
+        //'CSS Animations',
+    ],
     },
     img: '/vatsal-singh.jpg',
 };

0365fa79b2a59a0c8ad126aebffc19cf596126f7
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-05-08 22:39:35 +0800

    Create contact1.ts

---
+++ src/lib/content/contact1.ts
@@ -0,0 +1,12 @@
+import { author } from '@/lib/content/portfolio';
+import { ContactSectionType } from '@/lib/types/sections';
+
+export const contactSection: ContactSectionType = {
+    title: '',
+    subtitle: "",
+    paragraphs: [
+    '',
+    '',
+    ],
+    link: `https://wa.me/qr/K7MLAQDKNC6NL1`,
+};

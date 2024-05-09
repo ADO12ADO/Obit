4619e7bbb921f5aa4401692b15b284807d9561d6
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-05-09 17:02:30 +0800

    Update footer.ts

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -1,6 +1,6 @@
 import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
-    title: 'Design & Built by MUH.IKBAL',
+    title: 'Design & Built by OBIT',
     link: 'https://app-cv-sandy.vercel.app/',
 };

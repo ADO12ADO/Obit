730f9f51af10e029ae84b3bad5bd896f769dc4d1
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-05-09 17:23:39 +0800

    Update navbar.ts

--- src/lib/content/navbar.ts
+++ src/lib/content/navbar.ts
@@ -1,8 +1,13 @@
 import { NavbarSectionType } from '@/lib/types/sections';
+import { resumeFileName } from '@/lib/utils/config';
 
 export const navbarSection: NavbarSectionType = {
     navLinks: [
         { name: 'OBIT', url: '/#experience' },
         { name: 'SWAP', url: '/#contact' },
     ],
-}
+    cta: {
+        title: 'Download CV MUH.IKBAL',
+        url: 'https://twitter.com/muhammad_ikbal', // Ganti dengan URL Twitter Anda
+    },
+};
